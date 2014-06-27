07f5718ce47c86917193e838194e1d26be2b2eed
gajeev <gajeev@thesolutionjar.com>
2014-06-26 22:24:06 -0400

    Update engine.rb

--- lib/refinery/acts_as_indexed/engine.rb
+++ lib/refinery/acts_as_indexed/engine.rb
@@ -8,7 +8,7 @@ module Refinery
         ActiveSupport.on_load(:active_record) do
           require 'acts_as_indexed'
           ::ActsAsIndexed.configure do |config|
-            config.index_file = Rails.root.join('tmp', 'index')
+            config.index_file = Rails.root.join('tmp', 'index', get_tenant)
             config.index_file_depth = 3
             config.min_word_size = 3
           end
